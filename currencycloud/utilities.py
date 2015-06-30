5b28b37ee7330e28587ee334410eedc9cc29eb59
Alessandro Iob <alessandro.iob@gmail.com>
2015-06-30 17:49:13 +0200

    Real fix for UUID

--- currencycloud/utilities.py
+++ currencycloud/utilities.py
@@ -1,15 +1,7 @@
-import re
 import uuid
 
 __all__ = ['validate_uuid4']
 
-UUID_REGEX = re.compile(
-    "[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}",
-    re.I)
-
-
-def old_validate_uuid4(uuid_string):
-    return UUID_REGEX.match(uuid_string)
 
 def validate_uuid4(uuid_string):
     try:
