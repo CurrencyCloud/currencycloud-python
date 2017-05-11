034759d0db4fbf37ee46a27c61b5004a6ead4f6b
Francesco Boffa <fra.boffa@gmail.com>
2017-05-11 10:49:53 +0100

    Added tox to manage cross-version testing

---
+++ setup.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python
+# -*- encoding: utf-8 -*-
+
+from glob import glob
+from os.path import basename
+from os.path import splitext
+
+from setuptools import find_packages
+from setuptools import setup
+
+setup(
+    name='currencycloud',
+    version='2.0.0',
+    license='MIT',
+    description="Python SDK for the Currency Cloud API.",
+    long_description='',
+
+    author='Francesco Boffa',
+    author_email='francesco.boffa@currencycloud.com',
+    url='https://connect.currencycloud.com/documentation/getting-started/introduction',
+
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    py_modules=[splitext(basename(path))[0] for path in glob('src/*.py')],
+    include_package_data=True,
+
+    classifiers=[
+        "Development Status :: 2 - Pre-Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 2.6",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.2",
+        "Programming Language :: Python :: 3.3",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: Implementation :: PyPy",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+    ],
+
+    keywords=[],
+    install_requires=['requests'],
+    test_suite='tests'
+)
