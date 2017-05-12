fe7c03b34da548df260e0bc584e396de13c21c55
Francesco Boffa <fra.boffa@gmail.com>
2017-05-12 12:55:31 +0100

    Improved Settlements interface

--- src/currencycloud/resources/settlement.py
+++ src/currencycloud/resources/settlement.py
@@ -6,4 +6,30 @@ from .actions import DeleteMixin
 
 class Settlement(DeleteMixin, Resource):
     '''This class represents a CurrencyCloud Settlement'''
-    pass
+
+    def add_conversion(self, conversion_id):
+        response = self._client.add_conversion(self.id, conversion_id=conversion_id)
+        self.__update_attr(response)
+        return self
+
+    def remove_conversion(self, conversion_id):
+        response = self._client.remove_conversion(self.id, conversion_id=conversion_id)
+        self.__update_attr(response)
+        return self
+
+    def release(self):
+        response = self._client.release(self.id)
+        self.__update_attr(response)
+        return self
+
+    def unrelease(self):
+        response = self._client.unrelease(self.id)
+        self.__update_attr(response)
+        return self
+
+    def __update_attr(self, response):
+        self.conversion_ids = response['conversion_ids']
+        self.status = response['status']
+        self.entries = response['entries']
+        self.updated_at = response['updated_at']
+        self.released_at = response['released_at']
