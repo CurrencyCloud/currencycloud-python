2bb4e74ca10876805675482622296ed4bcf658cf
Ed Addario <ed.addario@currencycloud.com>
2018-06-08 06:51:55 +0100

    Update project and dependencies to latest versions

--- setup.py
+++ setup.py
@@ -10,9 +10,9 @@ from setuptools import setup
 
 setup(
     name='currency_cloud',
-    version='1.1.0',
+    version='2.2.0',
     license='MIT',
-    description="Python SDK for the Currency Cloud API.",
+    description="Python SDK for the Currencycloud API.",
     long_description='',
 
     author='Francesco Boffa',
@@ -25,23 +25,23 @@ setup(
     include_package_data=True,
 
     classifiers=[
-        "Development Status :: 2 - Pre-Alpha",
+        "Development Status :: 5 - Production/Stable",
         "Intended Audience :: Developers",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
         "Programming Language :: Python",
         "Programming Language :: Python :: 2",
-        "Programming Language :: Python :: 2.6",
         "Programming Language :: Python :: 2.7",
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.2",
         "Programming Language :: Python :: 3.3",
         "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: Implementation :: PyPy",
         "Topic :: Software Development :: Libraries :: Python Modules",
     ],
 
     keywords=[],
-    install_requires=['requests'],
+    install_requires=['requests', 'PyYAML', 'deprecation', 'pytest', 'mock', 'requests-mock', 'betamax', 'betamax-serializers'],
     test_suite='tests'
 )
