6cf597df1521562626727671ecaf9391bf331f9e
Ed <29247825+EAddario@users.noreply.github.com>
2018-11-10 11:07:05 +0000

    Update to v2.7.5

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ from setuptools import setup
 
 setup(
     name='currency_cloud',
-    version='2.3.0',
+    version='2.7.5',
     license='MIT',
     description="Python SDK for the Currencycloud API.",
     long_description='',
