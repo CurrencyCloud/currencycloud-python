13576cf4d51b03753a08104c042fc3649ed693b0
icebotariccl <Ilia.Cebotari@currencycloud.com>
2018-12-24 15:24:34 +0200

    Update test_accounts.py

--- tests/integration/test_accounts.py
+++ tests/integration/test_accounts.py
@@ -5,6 +5,7 @@ from currencycloud.resources import *
 
 
 class TestAccounts:
+
     def setup_method(self, method):
         # TODO: To run against real server please delete ../fixtures/vcr_cassettes/* and replace
         # login_id and api_key with valid credentials before running the tests
@@ -17,12 +18,33 @@ class TestAccounts:
     def test_accounts_can_get_current(self):
         with Betamax(self.client.config.session) as betamax:
             betamax.use_cassette('accounts/can_get_current')
-
             account = self.client.accounts.current()
-            assert account is not None
 
-            assert account.id == "8ec3a69b-02d1-4f09-9a6b-6bd54a61b3a8"
-            assert account.account_name == "Currency Cloud"
+            assert isinstance(account, Account)
+
+            assert account is not None
+            assert account.id == '8ec3a69b-02d1-4f09-9a6b-6bd54a61b3a8'
+            assert account.account_name == 'Currency Cloud'
+            assert account.brand == "currencycloud"
+            assert account.your_reference is None
+            assert account.status is not None
+            assert account.street is not None
+            assert account.city is not None
+            assert account.state_or_province is None
+            assert account.country is not None
+            assert account.postal_code is not None
+            assert account.spread_table is not None
+            assert account.legal_entity_type is not None
+            assert account.created_at is not None
+            assert account.updated_at is not None
+            assert account.identification_type is not None
+            assert account.identification_value is not None
+            assert account.short_reference is not None
+            assert account.api_trading is not None
+            assert account.online_trading is not None
+            assert account.phone_trading is not None
+            assert account.process_third_party_funds is not None
+            assert account.settlement_type is not None
 
     def test_accounts_can_find(self):
         with Betamax(self.client.config.session) as betamax:
@@ -38,7 +60,28 @@ class TestAccounts:
             assert account is not None
             assert isinstance(account, Account)
 
+            assert account.id is not None
+            assert account.account_name is not None
             assert account.brand == "currencycloud"
+            assert account.your_reference is None
+            assert account.status is not None
+            assert account.street is None
+            assert account.city is None
+            assert account.state_or_province is None
+            assert account.country is None
+            assert account.postal_code is None
+            assert account.spread_table is not None
+            assert account.legal_entity_type is None
+            assert account.created_at is None
+            assert account.updated_at is None
+            assert account.identification_type is None
+            assert account.identification_value is None
+            assert account.short_reference is not None
+            assert account.api_trading is not None
+            assert account.online_trading is not None
+            assert account.phone_trading is not None
+            assert account.process_third_party_funds is not None
+            assert account.settlement_type is not None
 
     def test_accounts_can_retrieve(self):
         with Betamax(self.client.config.session) as betamax:
@@ -50,6 +93,27 @@ class TestAccounts:
             assert isinstance(account, Account)
 
             assert account.id == "8ec3a69b-02d1-4f09-9a6b-6bd54a61b3a8"
+            assert account.account_name is not None
+            assert account.brand == "currencycloud"
+            assert account.your_reference == ''
+            assert account.status is not None
+            assert account.street is None
+            assert account.city is None
+            assert account.state_or_province is None
+            assert account.country is None
+            assert account.postal_code is None
+            assert account.spread_table == 'fxcg_rfx_default'
+            assert account.legal_entity_type is None
+            assert account.created_at is not None
+            assert account.updated_at is not None
+            assert account.identification_type is None
+            assert account.identification_value is None
+            assert account.short_reference is not None
+            assert account.api_trading is not None
+            assert account.online_trading is not None
+            assert account.phone_trading is not None
+            assert account.process_third_party_funds is not None
+            assert account.settlement_type is not None
 
     def test_accounts_can_create(self):
         with Betamax(self.client.config.session) as betamax:
@@ -66,6 +130,26 @@ class TestAccounts:
 
             assert account.id is not None
             assert account.account_name == "Currency Cloud Testing Environment"
+            assert account.brand == "currencycloud"
+            assert account.your_reference is None
+            assert account.status is not None
+            assert account.street is None
+            assert account.city is None
+            assert account.state_or_province is None
+            assert account.country == 'GB'
+            assert account.postal_code is None
+            assert account.spread_table == 'no_markup'
+            assert account.legal_entity_type == 'company'
+            assert account.created_at is not None
+            assert account.updated_at is not None
+            assert account.identification_type is None
+            assert account.identification_value is None
+            assert account.short_reference is not None
+            assert account.api_trading is not None
+            assert account.online_trading is not None
+            assert account.phone_trading is not None
+            assert account.process_third_party_funds is not None
+            assert account.settlement_type is not None
 
     def test_accounts_can_update(self):
         with Betamax(self.client.config.session) as betamax:
@@ -80,4 +164,26 @@ class TestAccounts:
 
             account = self.client.accounts.retrieve("8ec3a69b-02d1-4f09-9a6b-6bd54a61b3a8")
             assert account is not None
+
+            assert account.id == '8ec3a69b-02d1-4f09-9a6b-6bd54a61b3a8'
+            assert account.account_name == "Currency Cloud"
+            assert account.brand == "currencycloud"
+            assert account.your_reference == ''
+            assert account.status is not None
+            assert account.street is None
             assert account.city == "Manchester"
+            assert account.state_or_province is None
+            assert account.country is None
+            assert account.postal_code is None
+            assert account.spread_table == 'fxcg_rfx_default'
+            assert account.legal_entity_type is None
+            assert account.created_at is not None
+            assert account.updated_at is not None
+            assert account.identification_type is None
+            assert account.identification_value is None
+            assert account.short_reference is not None
+            assert account.api_trading is not None
+            assert account.online_trading is not None
+            assert account.phone_trading is not None
+            assert account.process_third_party_funds is not None
+            assert account.settlement_type is not None
