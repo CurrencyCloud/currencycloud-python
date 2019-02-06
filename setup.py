528d0a602dba4cbabb4fb601c732373f1bb47325
Ed Addario <ed.addario@currencycloud.com>
2019-02-06 10:32:47 +0000

    Update to v3.0.0

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ from setuptools import setup
 
 setup(
     name='currency_cloud',
-    version='2.7.5',
+    version='3.0.0',
     license='MIT',
     description="Python SDK for the Currencycloud API.",
     long_description='',
--- src/currencycloud/version.py
+++ src/currencycloud/version.py
@@ -1,1 +1,1 @@
-VERSION = '2.7.5'
+VERSION = '3.0.0'
