55f871a0ce1347acdeb1dee77f1b2ae8fb37dc2a
jonathancouchman <jonathan.couchman@currencycloud.com>
2022-07-12 15:03:21 +0100

    Adds User-Agent tests

--- src/currencycloud/http.py
+++ src/currencycloud/http.py
@@ -9,6 +9,8 @@ class Http(object):
     headers when necessary and poin to the appropriate host for the environment.
     '''
 
+    USER_AGENT = "CurrencyCloudSDK/2.0 Python/" + VERSION
+
     def __init__(self, config):
         self.config = config
         self.session = self.config.session
@@ -80,7 +82,7 @@ class Http(object):
 
     def __build_headers(self, authenticated):
 
-        headers = {"User-Agent": "CurrencyCloudSDK/2.0 Python/" + VERSION}
+        headers = {"User-Agent":self.USER_AGENT}
 
         if authenticated:
             headers["X-Auth-Token"] = self.config.auth_token
