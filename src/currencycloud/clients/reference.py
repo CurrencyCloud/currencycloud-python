e4c89f82b8ee145b237d8dee5b546cf5031cd6e9
Ricardo Gomes <ricardo.gomes@currencycloud.com>
2023-06-27 13:46:55 +0100

    SDKPY-83: Changes find to use post

--- src/currencycloud/clients/reference.py
+++ src/currencycloud/clients/reference.py
@@ -45,7 +45,7 @@ class Reference(Http):
 
     def bank_details(self, **kwargs):
         '''Returns the details of the bank related to the specified identifier.'''
-        response = self.post('/v2/reference/bank_details', query=kwargs)
+        response = self.post('/v2/reference/bank_details', kwargs)
         return BankDetails(self, **response)
 
     def payment_fee_rules(self, **kwargs):
